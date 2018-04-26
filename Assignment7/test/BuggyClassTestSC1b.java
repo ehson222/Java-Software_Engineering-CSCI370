f26f3edc4b6a5aefd53e3cb1edfd131639acfc7e
Jherico-Ryan Tapang <jhericot@outlook.com>
2018-04-26 08:41:49 -0400

    more method work

--- Assignment7/test/BuggyClassTestSC1b.java
+++ Assignment7/test/BuggyClassTestSC1b.java
@@ -1,5 +1,13 @@
+import edu.qc.seclass.BuggyClass;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class BuggyClassTestSC1b {
+    BuggyClass buggyClass;
+
+    @Test (expected = NullPointerException.class)
+    public void testBugMeth1(){
+        buggyClass.buggyMethod1(3, 2);
+        buggyClass.buggyMethod1(3, 0);
+    }
 }
